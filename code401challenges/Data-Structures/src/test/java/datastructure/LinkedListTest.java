1a1bda616de285a6451bb80a88b8ad1fe0eba634
Manish KC <kcmanish89@gmail.com>
2019-08-15 14:46:43 -0700

    Linked list completed

--- code401challenges/Data-Structures/src/test/java/datastructure/LinkedListTest.java
+++ code401challenges/Data-Structures/src/test/java/datastructure/LinkedListTest.java
@@ -28,19 +28,6 @@ public class LinkedListTest {
         list.insert(60);
 
 
-        list1.insert(2);
-        list1.insert(3);
-        list1.insert(1);
-
-
-
-        list2.insert(4);
-        list2.insert(9);
-        list2.insert(5);
-
-
-
-
     }
 
 
@@ -54,7 +41,7 @@ public class LinkedListTest {
     //Check to see if could be inserted into linked list
     @Test public void checkInsert1() {
         list.insert(10);
-        assertEquals("It should insert the value in the beginning of the list","HEAD -> 10 -> 60 -> 59 -> 37 -> 29 -> 23 -> 19 -> 17 -> 13 -> 7 -> 4 -> 1 -> NULL", list.toString());
+        assertEquals("It should insert the value in the beginning of the list","HEAD -> 1 -> 4 -> 7 -> 13 -> 17 -> 19 -> 23 -> 29 -> 37 -> 59 -> 60 -> 10 -> NULL", list.toString());
     }
 
     @Test public void checkInsert2() {
@@ -63,7 +50,7 @@ public class LinkedListTest {
         list.insert(30);
         list.insert(40);
 
-        assertEquals("It should insert multiple values in the beginning of the linked list in the order they were received","HEAD -> 40 -> 30 -> 20 -> 10 -> 60 -> 59 -> 37 -> 29 -> 23 -> 19 -> 17 -> 13 -> 7 -> 4 -> 1 -> NULL", list.toString());
+        assertEquals("It should insert multiple values in the beginning of the linked list in the order they were received","HEAD -> 1 -> 4 -> 7 -> 13 -> 17 -> 19 -> 23 -> 29 -> 37 -> 59 -> 60 -> 10 -> 20 -> 30 -> 40 -> NULL", list.toString());
     }
 
     @Test public void checkIncludes() {
@@ -73,13 +60,13 @@ public class LinkedListTest {
 
     @Test public void checkToString()
     {
-        assertEquals("It should return the string version of the linked list", "HEAD -> 60 -> 59 -> 37 -> 29 -> 23 -> 19 -> 17 -> 13 -> 7 -> 4 -> 1 -> NULL", list.toString());
+        assertEquals("It should return the string version of the linked list", "HEAD -> 1 -> 4 -> 7 -> 13 -> 17 -> 19 -> 23 -> 29 -> 37 -> 59 -> 60 -> NULL", list.toString());
     }
 
     //Challenge 06
     @Test public void addAtEndTest() {
         list.append(70);
-        assertEquals("It should append the value at the end of the linked list","HEAD -> 60 -> 59 -> 37 -> 29 -> 23 -> 19 -> 17 -> 13 -> 7 -> 4 -> 1 -> 70 -> NULL", list.toString());
+        assertEquals("It should append the value at the end of the linked list","HEAD -> 1 -> 4 -> 7 -> 13 -> 17 -> 19 -> 23 -> 29 -> 37 -> 59 -> 60 -> 70 -> NULL", list.toString());
 
     }
 
@@ -91,28 +78,28 @@ public class LinkedListTest {
         list.append(75);
         list.append(76);
         list.append(77);
-        assertEquals("It should add multiple values at the end of the linked list","HEAD -> 60 -> 59 -> 37 -> 29 -> 23 -> 19 -> 17 -> 13 -> 7 -> 4 -> 1 -> 71 -> 72 -> 73 -> 74 -> 75 -> 76 -> 77 -> NULL", list.toString());
+        assertEquals("It should add multiple values at the end of the linked list","HEAD -> 1 -> 4 -> 7 -> 13 -> 17 -> 19 -> 23 -> 29 -> 37 -> 59 -> 60 -> 71 -> 72 -> 73 -> 74 -> 75 -> 76 -> 77 -> NULL", list.toString());
 
     }
 
     @Test public void addInBeforeMiddleTest() {
         list.insertBefore(19,20);
-        assertEquals("It should add new value before the given value in the linked list","HEAD -> 60 -> 59 -> 37 -> 29 -> 23 -> 20 -> 19 -> 17 -> 13 -> 7 -> 4 -> 1 -> NULL", list.toString());
+        assertEquals("It should add new value before the given value in the linked list","HEAD -> 1 -> 4 -> 7 -> 13 -> 17 -> 20 -> 19 -> 23 -> 29 -> 37 -> 59 -> 60 -> NULL", list.toString());
     }
 
     @Test public void addInBeforeFirstTest() {
         list.insertBefore(60,20);
-        assertEquals("It should add new value before the given value in the linked list","HEAD -> 20 -> 60 -> 59 -> 37 -> 29 -> 23 -> 19 -> 17 -> 13 -> 7 -> 4 -> 1 -> NULL", list.toString());
+        assertEquals("It should add new value before the given value in the linked list","HEAD -> 1 -> 4 -> 7 -> 13 -> 17 -> 19 -> 23 -> 29 -> 37 -> 59 -> 20 -> 60 -> NULL", list.toString());
     }
 
     @Test public void addInAfterMiddleTest()  {
         list.insertAfter(19, 100);
-        assertEquals("It should add new value after the given value in the linked list","HEAD -> 60 -> 59 -> 37 -> 29 -> 23 -> 19 -> 100 -> 17 -> 13 -> 7 -> 4 -> 1 -> NULL", list.toString());
+        assertEquals("It should add new value after the given value in the linked list","HEAD -> 1 -> 4 -> 7 -> 13 -> 17 -> 19 -> 100 -> 23 -> 29 -> 37 -> 59 -> 60 -> NULL", list.toString());
     }
 
     @Test public void addInAfterLastTest() {
         list.insertAfter(1, 100);
-        assertEquals("It should add new value after the given value in the linked list","HEAD -> 60 -> 59 -> 37 -> 29 -> 23 -> 19 -> 17 -> 13 -> 7 -> 4 -> 1 -> 100 -> NULL", list.toString());
+        assertEquals("It should add new value after the given value in the linked list","HEAD -> 1 -> 100 -> 4 -> 7 -> 13 -> 17 -> 19 -> 23 -> 29 -> 37 -> 59 -> 60 -> NULL", list.toString());
     }
 
     /*
@@ -126,8 +113,8 @@ public class LinkedListTest {
 
     @Test public void kthFromEndTest() {
         System.out.println(list.toString());
-        assertEquals("It should return the last element of the linked list",1, list.kFromEnd(0));
-        assertEquals("It should return the first element of the linked list",60, list.kFromEnd(10));
+        assertEquals("It should return the last element of the linked list",60, list.kFromEnd(0));
+        assertEquals("It should return the first element of the linked list",1, list.kFromEnd(10));
         assertEquals("It should return -1 for argument equal to the length of linked list",-1, list.kFromEnd(11));
         assertEquals("It should return the last element of the linked list",-1, list.kFromEnd(-1));
         assertEquals("It should return the kth element from the last of the linked list",19, list.kFromEnd(5));
@@ -152,11 +139,25 @@ public class LinkedListTest {
 
 
     @Test public void mergeListTest() {
-        LinkedList l = new LinkedList();
-        System.out.println(list1.toString());
-        System.out.println(list2.toString());
 
-        System.out.println(l.mergeLists(list1, list2).toString());
+        // list 1
+        list1.insert(1);
+        list1.insert(3);
+        list1.insert(2);
+
+
+        // list 2
+        list2.insert(5);
+        list2.insert(9);
+        list2.insert(4);
+//        list2.insert(4);
+
+
+
+
+        assertEquals("HEAD -> 1 -> 5 -> 3 -> 9 -> 2 -> 4 -> NULL", list1.mergeLists(list1, list2).toString());
+
+//        assertEquals("", list1.mergeLists(list1, list2).toString());
 
     }
 
