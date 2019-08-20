3f5c60b46cec0e5c5a9c2c4e9190c9a4c0375372
Manish KC <kcmanish89@gmail.com>
2019-08-20 14:14:50 -0700

    lab stack and queue implementation

---
+++ code401challenges/stacksandqueues/src/main/java/stacksandqueues/Node.java
@@ -0,0 +1,10 @@
+package stacksandqueues;
+
+public class Node<T> {
+    T data;
+    Node next;
+
+    Node(T data) {
+        this.data = data;
+    }
+}
---
+++ code401challenges/stacksandqueues/src/main/java/stacksandqueues/Stack.java
@@ -0,0 +1,59 @@
+package stacksandqueues;
+
+public class Stack {
+    Node top;
+
+    //Constructor for empty stack
+    Stack() {
+        this.top = null;
+    }
+
+    //Check if the stack is empty
+    public boolean isEmpty() {
+        if (top == null) {
+            return true;
+        }
+        return false;
+    }
+
+    //Pushes new data into the stack
+    public <T> void push(T value) {
+        Node stackNode = new Node(value);
+
+        //Nothing has been in the stack
+        if (top == null) {
+            top = stackNode;
+        } else {
+            Node temp = top;
+            top = stackNode;
+            top.next = temp;
+        }
+    }
+
+    //Pop method
+    public <T> T pop() {
+        T poppedValue;
+        if(isEmpty()) {
+            poppedValue = (T) "Stack is empty";
+        } else {
+            poppedValue = (T) top.data;
+            top = top.next;
+        }
+        return poppedValue;
+    }
+
+    //Peek method
+    public <T> T peek() {
+        T peekedValue;
+        if(isEmpty()) {
+            peekedValue = (T) "Stack is empty";
+        } else {
+            peekedValue = (T) top.data;
+        }
+        return peekedValue;
+    }
+
+
+
+
+}
